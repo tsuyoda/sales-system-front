a4dfffd79b60156f6de0b44023e5e09f600b9a0e
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-10-10 14:42:35 -0300

    feat: create new interfaces

--- src/interfaces/IAuth.ts
+++ src/interfaces/IAuth.ts
@@ -1,4 +1,13 @@
-import { IUser, IUserLogin } from './IUser';
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface IUserLogin {
+  username: string;
+  password: string;
+}
 
 export interface IAuthContextData {
   user: IUser | null;
