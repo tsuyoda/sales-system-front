7c32b7cf0a167f845083de81f64feab38c7e0345
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-10-10 14:46:31 -0300

    feat: thrown error if api returns 401

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,26 @@
 import axios from 'axios';
+import { API_BASE_URL, APP_BASE_URL } from '../config/app';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7777'
+  baseURL: API_BASE_URL,
+  timeout: 10000
 });
+
+const responseSuccessHandler = (response: any) => {
+  return response;
+};
+
+const responseErrorHandler = (error: any) => {
+  if (window.location.href !== `${APP_BASE_URL}/login` && error.response.status === 401) {
+    localStorage.clear();
+    // eslint-disable-next-line no-alert
+    alert('Sessão expirada. Faça o login novamente.');
+    window.location.href = '/login';
+  }
+
+  return Promise.reject(error);
+};
+
+api.interceptors.response.use(responseSuccessHandler, responseErrorHandler);
 
 export default api;
