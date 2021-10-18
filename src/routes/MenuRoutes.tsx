58df5d1dee9bfe934d68f95c6414d7fd8c4ade7e
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-10-18 02:15:55 -0300

    feat: create User routes

--- src/routes/MenuRoutes.tsx
+++ src/routes/MenuRoutes.tsx
@@ -3,11 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
 import Product from '../components/Product';
 import User from '../components/User';
+import Register from '../components/User/Create';
+import Edit from '../components/User/Edit';
 
 function MenuRoutes() {
   return (
     <Switch>
       <Route path='/product' component={Product} />
+      <Route path='/user/register' component={Register} />
+      <Route path='/user/:id/edit' component={Edit} />
       <Route path='/user' component={User} />
       <Route path='/' component={Dashboard} />
     </Switch>
