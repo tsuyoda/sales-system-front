54f1f84eacf0accd4b6443806fd47c83a81e3656
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-10-22 02:02:46 -0300

    feat: create access control

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, createContext, useContext } from 'react';
 import { toast } from 'react-toastify';
 import { IAuthContextData, IAuthProviderProps, IUser, IUserLogin } from '../interfaces/IAuth';
+import { IPermission, IResource } from '../interfaces/IRole';
 
 import api from '../services/api';
 
@@ -8,31 +9,81 @@ const AuthContext = createContext<IAuthContextData>({} as IAuthContextData);
 
 function AuthProvider({ children }: IAuthProviderProps) {
   const [user, setUser] = useState<IUser | null>(null);
+  const [permissions, setPermissions] = useState<any | null>();
+  const [loadingPermissions, setLoadingPermissions] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const storedUser = sessionStorage.getItem('@SalesSystem:user');
+    const storedUser = JSON.parse(sessionStorage.getItem('@SalesSystem:user') || '{}');
     const storedToken = sessionStorage.getItem('@SalesSystem:token');
 
     if (storedUser && storedToken) {
-      setUser(JSON.parse(storedUser));
+      setUser(storedUser);
       api.defaults.headers.Authorization = `Bearer ${storedToken}`;
     }
 
     setLoading(false);
   }, []);
 
+  const accessControl = async (action: string, resource: string): Promise<boolean> => {
+    const response = await api.get('user/my_profile');
+
+    const { data: userData } = response.data;
+
+    if (userData.role.isAdmin) {
+      return true;
+    }
+
+    if (!userData.role.permissions) {
+      return false;
+    }
+
+    return userData.role.permissions
+      .map((permission: IPermission) => permission.resource.name === resource && permission.actions.includes(action))
+      .some((granted: boolean) => granted);
+  };
+
+  useEffect(() => {
+    if (!user) {
+      setPermissions(null);
+      setLoadingPermissions(false);
+      return;
+    }
+
+    setLoadingPermissions(true);
+
+    api.get(`/resource`).then(response => {
+      const { data: resources } = response.data;
+
+      const permissionsPromises = resources.map(async (resource: IResource) => {
+        const actionsPromises = resource.availableActions.map(async action => {
+          const permission = await accessControl(action, resource.name);
+
+          return [action, permission];
+        });
+
+        const actions = Object.fromEntries(await Promise.all(actionsPromises));
+
+        return [resource.name, actions];
+      });
+
+      Promise.all(permissionsPromises).then((grants: any[]) => {
+        setPermissions(Object.fromEntries(grants));
+        setLoadingPermissions(false);
+      });
+    });
+  }, [user]);
+
   const signIn = async ({ username, password }: IUserLogin) => {
     const {
       data: { data }
     } = await api.post('/auth', { username, password });
 
-    setUser(data.user);
-    api.defaults.headers.Authorization = `Bearer ${data.token}`;
-
     sessionStorage.setItem('@SalesSystem:user', JSON.stringify(data.user));
     sessionStorage.setItem('@SalesSystem:token', data.token);
-    sessionStorage.setItem('@SalesSystem:role', data.role.name);
+
+    api.defaults.headers.Authorization = `Bearer ${data.token}`;
+    setUser(data.user);
   };
 
   const signOut = () => {
@@ -44,9 +95,7 @@ function AuthProvider({ children }: IAuthProviderProps) {
   };
 
   const signed = () => {
-    const storedUser = sessionStorage.getItem('@SalesSystem:user');
-
-    if (storedUser) {
+    if (user) {
       return true;
     }
 
@@ -54,7 +103,9 @@ function AuthProvider({ children }: IAuthProviderProps) {
   };
 
   return (
-    <AuthContext.Provider value={{ user, signIn, signOut, signed }}>{loading ? null : children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signIn, signOut, signed, accessControl, loadingPermissions, permissions }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
   );
 }
 
--- src/interfaces/IAuth.ts
+++ src/interfaces/IAuth.ts
@@ -9,11 +9,20 @@ export interface IUserLogin {
   password: string;
 }
 
+export interface IAuthPermissions {
+  [key: string]: {
+    [key: string]: boolean;
+  };
+}
+
 export interface IAuthContextData {
   user: IUser | null;
+  loadingPermissions: boolean;
+  permissions: any;
   signIn({ username, password }: IUserLogin): Promise<void>;
   signOut(): void;
   signed(): boolean;
+  accessControl(action: string, resource: string): Promise<boolean>;
 }
 
 export interface IAuthProviderProps {
