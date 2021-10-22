5eee57a439ba31809847a9a91c597f741aa0cd4e
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-10-22 02:02:59 -0300

    feat: create new role routes

--- src/routes/MenuRoutes.tsx
+++ src/routes/MenuRoutes.tsx
@@ -3,16 +3,22 @@ import { Route, Switch } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
 import Product from '../components/Product';
 import User from '../components/User';
-import Register from '../components/User/Create';
-import Edit from '../components/User/Edit';
+import UserRegister from '../components/User/Create';
+import UserEdit from '../components/User/Edit';
+import Role from '../components/Role';
+import RoleRegister from '../components/Role/Create';
+import RoleEdit from '../components/Role/Edit';
 
 function MenuRoutes() {
   return (
     <Switch>
       <Route path='/product' component={Product} />
-      <Route path='/user/register' component={Register} />
-      <Route path='/user/:id/edit' component={Edit} />
+      <Route path='/user/register' component={UserRegister} />
+      <Route path='/user/:id/edit' component={UserEdit} />
       <Route path='/user' component={User} />
+      <Route path='/role/register' component={RoleRegister} />
+      <Route path='/role/:id/edit' component={RoleEdit} />
+      <Route path='/role' component={Role} />
       <Route path='/' component={Dashboard} />
     </Switch>
   );
