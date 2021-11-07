8ec703de0be4c513f05bbbf0bc84498eb6d5c62c
Lucas Toyoda <lucas.toyoda@eduzz.com>
2021-11-07 16:58:37 -0300

    feat: add new menu items

--- src/components/MenuItems/index.tsx
+++ src/components/MenuItems/index.tsx
@@ -1,21 +1,41 @@
-import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Collapse, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import PeopleIcon from '@material-ui/icons/People';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import PaymentIcon from '@material-ui/icons/Payment';
+import LocalAtmIcon from '@material-ui/icons/LocalAtm';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBox, faFileInvoice, faTruck, faUserShield } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBox,
+  faFileInvoice,
+  faTruck,
+  faUserShield,
+  faChartBar,
+  faLayerGroup,
+  faStar
+} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { useAuth } from '../../contexts/auth';
 import { useStyles } from './style';
 
 function MenuItems() {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
   const { loadingPermissions, permissions } = useAuth();
 
   return loadingPermissions ? null : (
     <List style={{ flexGrow: 1 }}>
+      <ListItem button component={Link} to='/'>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary='Dashboard' />
+      </ListItem>
       <ListItem button component={Link} to='/order' className={clsx({ [classes.hide]: !permissions?.orders?.read })}>
         <ListItemIcon>
           <PaymentIcon />
@@ -78,6 +98,57 @@ function MenuItems() {
         </ListItemIcon>
         <ListItemText primary='Nível de acesso' />
       </ListItem>
+      <ListItem
+        onClick={() => setOpen(prev => !prev)}
+        button
+        style={{ paddingRight: 0 }}
+        className={clsx({
+          [classes.hide]: !permissions?.scores?.read && !permissions?.scoreLevels?.read && !permissions?.benefits?.read
+        })}
+      >
+        <ListItemIcon>
+          <LocalAtmIcon />
+        </ListItemIcon>
+        <ListItemText primary='Programa de Pontos' />
+        {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+      </ListItem>
+      <Collapse in={open} timeout='auto' unmountOnExit>
+        <List component='div' disablePadding>
+          <ListItem
+            button
+            component={Link}
+            to='/score'
+            className={clsx({ [classes.hide]: !permissions?.scores?.read })}
+          >
+            <ListItemIcon>
+              <FontAwesomeIcon icon={faChartBar} size='lg' style={{ marginLeft: 5 }} />
+            </ListItemIcon>
+            <ListItemText primary='Pontuações' />
+          </ListItem>
+          <ListItem
+            button
+            component={Link}
+            to='/score-level'
+            className={clsx({ [classes.hide]: !permissions?.scoreLevels?.read })}
+          >
+            <ListItemIcon>
+              <FontAwesomeIcon icon={faLayerGroup} size='lg' style={{ marginLeft: 5 }} />
+            </ListItemIcon>
+            <ListItemText primary='Níveis' />
+          </ListItem>
+          <ListItem
+            button
+            component={Link}
+            to='/benefit'
+            className={clsx({ [classes.hide]: !permissions?.benefits?.read })}
+          >
+            <ListItemIcon>
+              <FontAwesomeIcon icon={faStar} size='lg' style={{ marginLeft: 4 }} />
+            </ListItemIcon>
+            <ListItemText primary='Benefícios' />
+          </ListItem>
+        </List>
+      </Collapse>
     </List>
   );
 }
